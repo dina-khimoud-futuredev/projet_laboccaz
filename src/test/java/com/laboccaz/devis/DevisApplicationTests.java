a07b87f785333b4607bfd179828108932e9f5874
Dina Khimoud <dinakhimoud03@gmail.com>
2026-06-01 15:12:08 +0200

    Initialisation du projet Spring Boot

---
+++ src/test/java/com/laboccaz/devis/DevisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboccaz.devis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
